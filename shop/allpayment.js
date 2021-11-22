e6c689cce7c5d203722895fbca2eb0bf8a47ef28
VENOM-BOTZ <botzvenomofficial@gmail.com>
2021-11-22 22:14:13 +0700

    Alphab0t

---
+++ shop/allpayment.js
@@ -0,0 +1,24 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY REVI GANZ
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ QRIS SCAN DI ATAS
+2. QRIS ALL PAY
+	ㅁ TANYA OWNER
+3. DANA
+	ㅁ 087772961550
+	
+Semuanya atas nama N****A
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,17 @@
+const donasibot = () => {
+return `DONASI BISA VIA QRIS ALL PAY / E-MONEY
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ QRIS DI ATAS
+2. QRIS ALL PAY
+	ㅁ TANYA OWNER
+3. DANA
+	ㅁ 087772961550
+	
+Semuanya atas nama RE****V
+`
+	}
+
+exports.donasibot = donasibot
